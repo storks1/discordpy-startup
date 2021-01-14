ec070488f97d8d53477d5fa85855aaa12cae89ae
storks1 <62281949+storks1@users.noreply.github.com>
2021-01-15 00:42:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,17 +7,6 @@ import re
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 client = discord.Client()
 set = 0
 word = ("ばなな")
